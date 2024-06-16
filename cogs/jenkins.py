c15e7b7f6ac110a2a21020beb104744d8a5d3dce
lifehackerhansol <lifehackerhansol@ds-homebrew.com>
2024-06-15 23:33:21 -0700

    jenkins: add help message

--- cogs/jenkins.py
+++ cogs/jenkins.py
@@ -39,6 +39,11 @@ class Jenkins(commands.Cog):
 
     @commands.command()
     async def build(self, ctx, device: str, branch: str, release_type: str, manifest: Optional[str] = ""):
+        """
+        Start a LineageOS build.
+        Usage: `build <device> <branch> <release_type> [manifest (if not hosted on hudson)]`
+        Example: build joan lineage-21.0 testing
+        """
         async with ctx.typing():
             local_manifest_url = urllib.parse.quote_plus(manifest if manifest else f"https://raw.githubusercontent.com/lifehacker-101/hudson/main/manifests/{branch}/devices/{device}.xml")
             build_trigger_url = f"{self.bot.config['JENKINS_URL']}/job/lineageos/job/{branch}/buildWithParameters?JENKINS_DEVICE={device}&JENKINS_LOCAL_MANIFEST={local_manifest_url}{f'&JENKINS_RELEASE_TYPE={release_type}' if release_type != 'testing' else ''}"
